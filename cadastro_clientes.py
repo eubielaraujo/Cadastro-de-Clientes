c268f031a6bb541580cc753eec58931f71d28f4e
Gabriel Araujo <131310444+ByPadox@users.noreply.github.com>
2025-02-12 18:10:16 -0300

    Add files via upload

--- cadastro_clientes.py
+++ cadastro_clientes.py
@@ -1,80 +1,133 @@
-from tkinter import *
+import customtkinter as ctk
 import sqlite3 as sql
 import pandas as pd
 import os
 
+#Caminhos Banco de Dados e Excel 
 sys_dir1 = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(sys_dir1, 'clientes.db')
 exc_path = os.path.join(sys_dir1, 'clientes.xlsx')    
 
-janela = Tk()
-janela.title('Cadastro de Clientes')
-icon_path2 = os.path.join(sys_dir1, "ICONE.ico")
-janela.iconbitmap(icon_path2)
-janela.geometry()
+ctk.set_appearance_mode('light')
 
-def cadastrar_clientes():
-    conexao = sql.connect('clientes.db')
-    c = conexao.cursor()
+login = ctk.CTk()
+login.title('LOGIN')
+icon_path1 = os.path.join(sys_dir1, "LOG_ICO.ICO")  # Caminho relativo
+login.iconbitmap(icon_path1)
+login.geometry()
 
-    #Inserir dados no banco de dados
-    c.execute("INSERT INTO clientes VALUES (:nome,:sobrenome,:email,:telefone)",
-              {
-                  'nome': entry_nome.get(),
-                  'sobrenome': entry_sobrenome.get(),
-                  'email': entry_email.get(),
-                  'telefone': entry_telefone.get()
-              })
-    conexao.commit()
-    conexao.close()
+def janela_cadastro():
 
-    entry_nome.delete(0,"end")
-    entry_sobrenome.delete(0,"end")
-    entry_email.delete(0,"end")
-    entry_telefone.delete(0,"end")
+    janela = ctk.CTk()
+    janela.title('Cadastro de Clientes')
+    icon_path2 = os.path.join(sys_dir1, "ICONE.ICO")  # Caminho relativo
+    janela.iconbitmap(icon_path2)
+    janela.geometry('960x540')
 
-def exportar_clientes():
-    conexao = sql.connect(db_path)
-    c = conexao.cursor()
 
-    #Exportar dados para o Excel
-    c.execute("SELECT *, oid FROM clientes")
-    clientes_cadastrados = c.fetchall()
-    clientes_cadastrados = pd.DataFrame(clientes_cadastrados, columns= ['nome','sobrenome','email','telefone','Id_banco'])
-          
-    clientes_cadastrados.to_excel(exc_path)
+    def cadastrar_clientes():
+        conexao = sql.connect(db_path)
+        c = conexao.cursor()
 
-    conexao.commit()
-    conexao.close()
+        #Inserir dados no banco de dados
+        c.execute("INSERT INTO clientes VALUES (:nome,:sobrenome,:email,:telefone)",
+                {
+                    'nome': entry_nome.get(),
+                    'sobrenome': entry_sobrenome.get(),
+                    'email': entry_email.get(),
+                    'telefone': entry_telefone.get()
+                })
+        conexao.commit()
+        conexao.close()
 
-label_nome = Label(janela, text='Nome')
-label_nome.grid(row=0, column=0, padx=10, pady=10)
+        entry_nome.delete(0,"end")
+        entry_sobrenome.delete(0,"end")
+        entry_email.delete(0,"end")
+        entry_telefone.delete(0,"end")
 
-label_sobrenome = Label(janela, text='Sobrenome')
-label_sobrenome.grid(row=1, column=0, padx=10, pady=10)
+    def exportar_clientes():
+        conexao = sql.connect(db_path)
+        c = conexao.cursor()
 
-label_email = Label(janela, text='Email')
-label_email.grid(row=2, column=0, padx=10, pady=10)
+        #Exportar dados para o Excel
+        c.execute("SELECT *, oid FROM clientes")
+        clientes_cadastrados = c.fetchall()
+        clientes_cadastrados = pd.DataFrame(clientes_cadastrados, columns= ['nome','sobrenome','email','telefone','Id_banco'])
+    
+        clientes_cadastrados.to_excel(exc_path)
 
-label_telefone = Label(janela, text='Telefone')
-label_telefone.grid(row=3, column=0, padx=10, pady=10)
+        conexao.commit()
+        conexao.close()
 
-entry_nome = Entry(janela, width =35)
-entry_nome.grid(row=0,column=1,padx=10,pady=10)
+    label_nome = ctk.CTkLabel(janela, text='Nome')
+    label_nome.pack(padx=10, pady=5)
 
-entry_sobrenome = Entry(janela, width =35)
-entry_sobrenome.grid(row=1,column=1,padx=10,pady=10)
+    entry_nome = ctk.CTkEntry(janela,placeholder_text='Digite seu Nome', width = 200,border_color='cyan', font=ctk.CTkFont(family='Times'), justify='center')
+    entry_nome.pack(padx=0,pady=0)
 
-entry_email = Entry(janela, width =35)
-entry_email.grid(row=2,column=1,padx=10,pady=10)
+    label_sobrenome = ctk.CTkLabel(janela, text='Sobrenome')
+    label_sobrenome.pack(padx=10, pady=5)
 
-entry_telefone = Entry(janela, width =35)
-entry_telefone.grid(row=3,column=1,padx=10,pady=10)
+    entry_sobrenome = ctk.CTkEntry(janela,placeholder_text='Digite seu Sobrenome', width = 200, border_color='cyan', font=ctk.CTkFont(family='Times'), justify='center')
+    entry_sobrenome.pack(padx=0,pady=0)
 
-botao_cadastrar = Button(text='Cadastrar Cliente', command=cadastrar_clientes)
-botao_cadastrar.grid(column=0,row=4,columnspan=2,padx=10,pady=10,ipadx = 80)
+    label_email = ctk.CTkLabel(janela, text='Email')
+    label_email.pack(padx=10, pady=5)
 
-botao_exportar = Button(text='Exportar para Excel', command=exportar_clientes)
-botao_exportar.grid(column=0,row=5,columnspan=2,padx=10,pady=10,ipadx = 80)
+    entry_email = ctk.CTkEntry(janela,placeholder_text='Digite seu Email', width = 200, border_color='cyan',font=ctk.CTkFont(family='Times'), justify='center')
+    entry_email.pack(padx=0,pady=0)
 
-janela.mainloop()
+    label_telefone = ctk.CTkLabel(janela, text='Telefone')
+    label_telefone.pack(padx=10, pady=5)
+
+    entry_telefone = ctk.CTkEntry(janela,placeholder_text='Digite seu Telefone', width = 200, border_color='cyan',font=ctk.CTkFont(family='Times'), justify='center')
+    entry_telefone.pack(padx=0,pady=5)
+
+    botao_cadastrar = ctk.CTkButton(janela, text='Cadastrar Cliente', command=cadastrar_clientes)
+    botao_cadastrar.pack(padx=10,pady=10,ipadx = 100)
+
+    botao_exportar = ctk.CTkButton(janela, text='Exportar para Excel', command=exportar_clientes)
+    botao_exportar.pack(padx=10,pady=0 ,ipadx = 100)
+
+    botao_sair = ctk.CTkButton(janela, text='Sair', command=janela.destroy)
+    botao_sair.pack(padx=10,pady=10 ,ipadx = 100)
+
+    janela.mainloop(0)
+
+incorreto = ctk.CTkLabel(login, text="", text_color='red')
+
+def logar():
+    login_usuario = 'admin'
+    login_senha = '123'
+
+    preencher1 = preencher_usuario.get()
+    preencher2 = preencher_senha.get()
+    
+    if preencher1 == login_usuario and preencher2 == login_senha:
+        login.destroy(),
+        janela_cadastro()
+    else:
+        incorreto.configure(text="Senha e/ou Usuário Incorretos!")
+        incorreto.pack(padx=10,pady=5)
+
+usuario = ctk.CTkLabel(login, text='Usuário')
+usuario.pack(padx=10, pady=5)
+
+preencher_usuario = ctk.CTkEntry(login,placeholder_text='Digite seu Usuário', width = 200,border_color='cyan', font=ctk.CTkFont(family='Times'), justify='center')
+preencher_usuario.pack(padx=0,pady=0)
+
+senha = ctk.CTkLabel(login, text='Senha')
+senha.pack(padx=10, pady=5)
+
+preencher_senha = ctk.CTkEntry(login,placeholder_text='Digite sua Senha', width = 200,border_color='cyan', font=ctk.CTkFont(family='Times'), justify='center')
+preencher_senha.pack(padx=0,pady=0)
+
+botao_cadastrar = ctk.CTkButton(login, text='Logar', command=logar)
+botao_cadastrar.pack(padx=10,pady=10,ipadx = 100)
+
+botao_sair = ctk.CTkButton(login, text='Sair', command=login.destroy)
+botao_sair.pack(padx=10,pady=10,ipadx = 100)
+
+login.mainloop()
+
+# parar de aparecer varios incorretos / conexao correta do bd e excel 
